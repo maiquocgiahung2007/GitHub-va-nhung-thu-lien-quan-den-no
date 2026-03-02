86c05e007119773953c989a4c3de7fbfc0923514
maihung <maiquocgiahung2007@gmail.com>
2026-03-02 21:39:14 +0700

    feat(maihung): changed name
    -add irl name
    
    Refs:#3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Roblox Enjoyer at FPT")
+Console.WriteLine("Roblox Enjoyer at FPT, Mai Quoc Gia Hung")
